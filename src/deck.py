96ffa740f923e8c0bfbc8518fb73054a86710537
jamiejay2003 <74116299+jukesbox@users.noreply.github.com>
2025-01-13 13:35:08 +0000

    Deck Tests
    Implemented some tests for the Deck class, and implementation of the class

--- src/deck.py
+++ src/deck.py
@@ -1,9 +1,52 @@
 """
 This class will have the properties of the deck.
 """
-
+from collections import deque
+from src.card import Card
+from random import shuffle
+SUITS = ["Diamonds", "Clubs", "Spades", "Hearts"]
+RANKS = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
 
 class Deck:
-    def __init__(self):
+    def __init__(self, packs):
+        self._card_stack = deque()
+        self._num_packs = packs
+        self.create_deck()
         pass
+
+    def add_card(self, card):
+        self._card_stack.append(card)
+
+
+    def get_card(self):
+        if int(self.get_num_cards()) < 1:
+            raise IndexError("Deck is empty - no more cards to draw")
+        else:
+            return self._card_stack.pop()
+
+    def shuffle_cards(self, cards):
+        shuffle(cards)
+        return cards
+
+    def create_card_list(self):
+        all_cards = []
+        for i in range(self._num_packs):
+            for suit in SUITS:
+                for rank in RANKS:
+                    all_cards.append(Card(rank, suit))
+        return all_cards
+
+    def create_deck(self, cards_to_add=None):
+        if cards_to_add == None:
+            cards_to_add = self.create_card_list()
+        for card in self.shuffle_cards(cards_to_add):
+            self.add_card(card)
+
+    def set_deck(self, card_list):
+        self._card_stack.clear()
+        self.create_deck(card_list)
+        
+
+    def get_num_cards(self):
+        return len(self._card_stack)
 
--- test/test_deck.py
+++ test/test_deck.py
@@ -4,11 +4,37 @@ Include getter/setters
 """
 
 from src.deck import Deck
+from src.card import Card
 import unittest
 
 class DeckTestCase(unittest.TestCase):
     def setUp(self):
+        num_packs = 1
+        self.deck = Deck(num_packs)
         pass
     
     def tearDown(self):
         pass
+
+    def test_add_card(self):
+        old_size = self.deck.get_num_cards()
+        self.deck.add_card(Card("Ace", "Spades"))
+        self.assertEqual(old_size+1, self.deck.get_num_cards())
+
+    def test_num_cards(self):
+        self.assertEqual(self.deck.get_num_cards(), 52)
+
+    def test_num_cards_2packs(self):
+        new_deck = Deck(2)
+        self.assertEqual(new_deck.get_num_cards(), 52*2)
+
+    def test_get_card(self):
+        card_to_add = Card("Ace", "Hearts")
+        self.deck.add_card(card_to_add)
+        self.assertEqual(card_to_add, self.deck.get_card())
+
+    def test_get_card_empty(self):
+        with self.assertRaises(IndexError):
+            self.deck.set_deck([]) # set the deck to be empty
+            self.deck.get_card()
+
